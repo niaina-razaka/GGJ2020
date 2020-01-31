73a2c63df6d9676dbd8473be501f307ce6429b39
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2020-02-01 00:01:43 +0300

    Revert "World"
    This reverts commit 2481bd61665deb231ea7f9cfb549eea0d8ed66a3.

--- Assets/GGJ2020/Scripts/Managers/GameManager.cs
+++ Assets/GGJ2020/Scripts/Managers/GameManager.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
-    public enum Element
-    {
-        WATER,
-        FIRE,
-        WIND,
-        EARTH
-    }
-
-    [Header("Game Manager")]
-    public Element WorldElement = Element.WATER;
-
     // Start is called before the first frame update
-    protected void Start()
+    void Start()
     {
-
+        
     }
 
     // Update is called once per frame
-    protected void Update()
+    void Update()
     {
         
     }
--- Assets/GGJ2020/Scripts/Managers/LevelManager.cs
+++ Assets/GGJ2020/Scripts/Managers/LevelManager.cs
@@ -1,61 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class LevelManager : GameManager
+public class LevelManager : MonoBehaviour
 {
-    [Header("Level Manager")]
-    public float animationDelay = 0.15f;
-    public List<WorldCube> cubes = new List<WorldCube>();
-
-    new private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        base.Start();
-        cubes = FindObjectsOfType<WorldCube>().ToList();
-        //order cubes
-        cubes = cubes.OrderBy(x => x.transform.localPosition.x).ThenBy(x => x.transform.position.y).ToList();
+        
     }
 
-    new private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        base.Update();
-    }
-
-    private void OnGUI()
-    {
-        if (GUI.Button(new Rect(0, 0, 100, 25), "WATER"))
-        {
-            ChangeElement(Element.WATER);
-        }
-        if (GUI.Button(new Rect(0, 25, 100, 25), "FIRE"))
-        {
-            ChangeElement(Element.FIRE);
-        }
-        if (GUI.Button(new Rect(0, 50, 100, 25), "WIND"))
-        {
-            ChangeElement(Element.WIND);
-        }
-        if (GUI.Button(new Rect(0, 75, 100, 25), "EARTH"))
-        {
-            ChangeElement(Element.EARTH);
-        }
-    }
-
-    public void ChangeElement(Element element)
-    {
-        WorldElement = element;
-        StartCoroutine(DynamicAnimateCubes(WorldElement));
-    }
-
-    IEnumerator DynamicAnimateCubes(Element element)
-    {
-        int count = 0;
-        while (count < cubes.Count)
-        {
-            yield return new WaitForSeconds(animationDelay);
-            cubes[count].Element = element;
-            count++;
-        }
+        
     }
 }
