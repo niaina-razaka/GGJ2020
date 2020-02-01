5cefc277e460093ec08b4b1e7fbd3d0149e88cbb
Unknown <sandratraniaina@gmail.com>
2020-02-01 17:36:34 +0300

    mande ny mask loupe

---
+++ Assets/GGJ2020/Scripts/Controllers/GlassController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassController : MonoBehaviour
+{
+
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z = 0;
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+}
