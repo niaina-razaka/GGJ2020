27e755d1876b1e1037cef506e394cfe9433e8bff
Unknown <sandratraniaina@gmail.com>
2020-02-01 14:35:15 +0300

    setting base cam test levelDesign

--- Assets/GGJ2020/Scripts/Controllers/CameraController.cs
+++ Assets/GGJ2020/Scripts/Controllers/CameraController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x_axis = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(x_axis, 0);
+        rb2d.velocity = movement * speed;
     }
 }
