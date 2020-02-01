4b8ba29c524897c793765c7262e851412bfcd5a7
Mahery Rakotondrazaka <maheryrakotondrazaka@gmail.com>
2020-02-01 15:58:02 +0300

    well test

--- Assets/GGJ2020/Scripts/Entities/Player.cs
+++ Assets/GGJ2020/Scripts/Entities/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [Serializable]
 public class Player: MonoBehaviour
 {
-    public int Life { get; set; } = 5;
+    public int Life { get; set; } = 4;
     public float Energy { get; set; } = 3;
     public string name { get; private set; } = "Hafah";
 }
