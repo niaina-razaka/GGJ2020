ba12af16c58df2d895d96970abeaf558f797d636
Unknown <sandratraniaina@gmail.com>
2020-02-02 10:51:29 +0300

    sound manager

--- Assets/GGJ2020/Scripts/Managers/AudioManager.cs
+++ Assets/GGJ2020/Scripts/Managers/AudioManager.cs
@@ -6,7 +6,22 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioClip clipBoss;
+    public AudioClip clipLevel;
     public List<MyAudio> audios = new List<MyAudio>();
+
+    private void Start()
+    {
+        SwitchToLevel();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SwitchToBoss();
+        }
+    }
 
     [Serializable]
     public class MyAudio
@@ -24,5 +39,17 @@ public class AudioManager : MonoBehaviour
                 break;
             }
         }
+    }
+
+    public void SwitchToBoss()
+    {
+        audioSource.clip = clipBoss;
+        audioSource.Play();
+    }
+
+    public void SwitchToLevel()
+    {
+        audioSource.clip = clipLevel;
+        audioSource.Play();
     }
 }
